9def7cd80b460781f7bbdd10811f783fbccfeb9c
revathy <revathy@gmail.com>
2024-01-22 07:19:47 +0530

    active link created

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'angluar-practice' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angluar-practice');
+    expect(app.title).toEqual('angluar-directives');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angluar-practice');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angluar-directives');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet,RouterLinkActive,Router} from '@angular/router';
+import { RouterOutlet,RouterLink,RouterLinkActive,Router} from '@angular/router';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,RouterLinkActive],
+  imports: [CommonModule,RouterOutlet,RouterLinkActive,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
